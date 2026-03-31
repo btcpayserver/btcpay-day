a4355861357850d9d1ccaf725a4cf3a69e42ec60
Pavlenex <36959754+pavlenex@users.noreply.github.com>
2026-03-31 17:47:41 +0200

    add registration link

--- src/data/event.ts
+++ src/data/event.ts
@@ -5,7 +5,7 @@ export const EVENT = {
   address:    'Teplická 492, 190 00 Praha 9',
   city:       'Prague, Czech Republic',
   mapsUrl:    'https://maps.google.com/?q=Hotel+Duo+Praha+Teplická+492',
-  ticketUrl:  '#register',
+  ticketUrl:  'https://pay.pavlenex.com/apps/2SfptYNoGkDKoc5Sc5C2s6jXpQWD/pos',
   telegramUrl: 'https://t.me/+h9RyKmiXBdhhM2I0',
   youtubeHeroId: 'rIjNPuYxVMo',
   youtubeRigaId: 'JWZPN-SAO3U',
